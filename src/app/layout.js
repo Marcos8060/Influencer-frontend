5162f44a1930db8af2ade743cd998ea10a0dd649
Marcos8060 <marcosgav80@gmail.com>
2025-02-04 23:41:23 +0300

    mde changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 'use client'
-import "../styles/globals.css";
+import "./globals.css";
 import { PrimeReactProvider } from "primereact/api";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Influencer Platform</title>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
       </head>
       <body>
         <PrimeReactProvider>
