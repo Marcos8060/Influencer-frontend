0b4cd19fc70e6673a077081c99baaefd34c9bb40
Marcos8060 <marcosgav80@gmail.com>
2025-02-04 21:26:59 +0300

    made chnges

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        appDir: true, // Ensure this is enabled
+      },
+};
 
 export default nextConfig;
