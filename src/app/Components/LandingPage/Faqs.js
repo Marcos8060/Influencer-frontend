f0aa5e4bdc4badf9db510cb5479fe0d3d737fce8
Marcos8060 <marcosgav80@gmail.com>
2025-02-05 16:33:58 +0300

    modified the faqs

--- src/app/Components/LandingPage/Faqs.js
+++ src/app/Components/LandingPage/Faqs.js
@@ -1,48 +1,44 @@
-
-import React from 'react';
-import { Accordion, AccordionTab } from 'primereact/accordion';
+import React, { useState } from "react";
+import { IoIosArrowForward, IoIosArrowDown } from "react-icons/io";
+import { faqsData } from "./faqsdata";
 
 export default function Faqs() {
-    return (
-        <section className='w-7/12 mx-auto'>
-            <h1 className='uppercase text-3xl font-bold text-center mb-8'>Faq</h1>
-        <div className="card h-screen">
-            <Accordion activeIndex={0}>
-                <AccordionTab header="How do you clone a Webflow Template from the Showcase?">
-                    <p className="m-0">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                </AccordionTab>
-                <AccordionTab header="How do you clone a Webflow Template from the Showcase?">
-                    <p className="m-0">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa 
-                        quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
-                        sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
-                        Consectetur, adipisci velit, sed quia non numquam eius modi.
-                    </p>
-                </AccordionTab>
-                <AccordionTab header="How do you clone a Webflow Template from the Showcase?">
-                    <p className="m-0">
-                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
-                        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt 
-                        mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. 
-                        Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-                    </p>
-                </AccordionTab>
-                <AccordionTab header="How do you clone a Webflow Template from the Showcase?">
-                    <p className="m-0">
-                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
-                        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt 
-                        mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. 
-                        Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-                    </p>
-                </AccordionTab>
-            </Accordion>
+  const [openIndex, setOpenIndex] = useState(0); 
+
+  const toggleShow = (index) => {
+    setOpenIndex((prevIndex) => (prevIndex === index ? null : index)); 
+  };
+
+  return (
+    <div className="h-screen py-8">
+      <section className="w-8/12 mx-auto">
+        <h1 className="uppercase text-3xl font-bold text-center mb-8">FAQs</h1>
+        <div className="bg-background rounded p-8 space-y-4">
+          {faqsData.map((item, index) => (
+            <div key={index} className="bg-white shadow-xl rounded-xl p-4">
+              <div className="flex items-center justify-between">
+                <h1 className="text-primary">{item.title}</h1>
+                {openIndex === index ? (
+                  <IoIosArrowDown
+                    onClick={() => toggleShow(index)}
+                    className="bg-primary text-white rounded-full text-3xl p-2 cursor-pointer"
+                  />
+                ) : (
+                  <IoIosArrowForward
+                    onClick={() => toggleShow(index)}
+                    className="bg-primary text-white rounded-full text-3xl p-2 cursor-pointer"
+                  />
+                )}
+              </div>
+              {openIndex === index && (
+                <div className="w-10/12">
+                  <p className="font-normal mt-2 text-sm">{item.description}</p>
+                </div>
+              )}
+            </div>
+          ))}
         </div>
-        </section>
-    )
+      </section>
+    </div>
+  );
 }
-        
---
+++ src/app/Components/LandingPage/faqsdata.js
@@ -0,0 +1,34 @@
+export const faqsData = [
+    {
+        title: "Why should I be a part of the Influencer Platform?",
+        description: `While the copany wishes they had more time to work out the kinks
+            with the Teamollo, they are quite happy with the result of the
+            project. The resulting website that the teamdevoloped is fast and
+            the communication with the vender was very good. The company will
+            work with them again.`
+    },
+    {
+        title: "Why should I be a part of the Influencer Platform?",
+        description: `While the copany wishes they had more time to work out the kinks
+            with the Teamollo, they are quite happy with the result of the
+            project. The resulting website that the teamdevoloped is fast and
+            the communication with the vender was very good. The company will
+            work with them again.`
+    },
+    {
+        title: "Why should I be a part of the Influencer Platform?",
+        description: `While the copany wishes they had more time to work out the kinks
+            with the Teamollo, they are quite happy with the result of the
+            project. The resulting website that the teamdevoloped is fast and
+            the communication with the vender was very good. The company will
+            work with them again.`
+    },
+    {
+        title: "Why should I be a part of the Influencer Platform?",
+        description: `While the copany wishes they had more time to work out the kinks
+            with the Teamollo, they are quite happy with the result of the
+            project. The resulting website that the teamdevoloped is fast and
+            the communication with the vender was very good. The company will
+            work with them again.`
+    },
+]
