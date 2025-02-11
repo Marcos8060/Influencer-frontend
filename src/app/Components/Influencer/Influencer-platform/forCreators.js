31dbfdecf53cfb5db81adfb282798092fd61208f
Marcos8060 <marcosgav80@gmail.com>
2025-02-11 16:05:26 +0300

    made changes

---
+++ src/app/Components/Influencer/Influencer-platform/forCreators.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+const ForCreators = () => {
+  return (
+    <section className="my-4 md:px-12 px-4">
+      <div className="bg-background py-4 px-8 rounded-xl">
+        <h3 className="font-normal">For Creators</h3>
+        <h2 className="font-bold text-xl">
+          Unlock Unlimited Opportunities Through Influencer platform
+        </h2>
+        <small className="font-light">
+          Use this section to describe your company and the products you offer.
+          You could share your companys story and details about why you are in
+          business
+        </small>
+
+        <section className="flex items-center gap-4 my-6">
+          <div className="border border-primary rounded-xl p-4 bg-white">
+            <h6 className="font-semibold">Showcase your authenticity</h6>
+            <small className="text-xs">
+              Your creative journey is as unique as your fingerprint, and at
+              Influencer platform, we celebrate that individuality. Build a
+              personalized profile that reflects your style, personality, and
+              creative ethos. Showcase your best work, share your story, and let
+              your authenticity shine through. Whether you're a visual artist,
+              musician, writer, or any other creative professional, Influencer
+              platform provides a platform for you to express yourself
+              authentically and connect with an audience that appreciates your
+              unique voice.
+            </small>
+          </div>
+          <div className="border border-primary rounded-xl p-4 bg-white">
+            <h6 className="font-semibold">Showcase your authenticity</h6>
+            <small className="text-xs">
+              Your creative journey is as unique as your fingerprint, and at
+              Influencer platform, we celebrate that individuality. Build a
+              personalized profile that reflects your style, personality, and
+              creative ethos. Showcase your best work, share your story, and let
+              your authenticity shine through. Whether you're a visual artist,
+              musician, writer, or any other creative professional, Influencer
+              platform provides a platform for you to express yourself
+              authentically and connect with an audience that appreciates your
+              unique voice.
+            </small>
+          </div>
+          <div className="border border-primary rounded-xl p-4 bg-white">
+            <h6 className="font-semibold">Showcase your authenticity</h6>
+            <small className="text-xs">
+              Your creative journey is as unique as your fingerprint, and at
+              Influencer platform, we celebrate that individuality. Build a
+              personalized profile that reflects your style, personality, and
+              creative ethos. Showcase your best work, share your story, and let
+              your authenticity shine through. Whether you're a visual artist,
+              musician, writer, or any other creative professional, Influencer
+              platform provides a platform for you to express yourself
+              authentically and connect with an audience that appreciates your
+              unique voice.
+            </small>
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+};
+
+export default ForCreators;
--- src/app/influencer/page.js
+++ src/app/influencer/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import InfluencerHeroSection from "../Components/Influencer/Influencer-platform/heroSection";
 import Slider from "../Components/Influencer/Influencer-platform/slider";
+import ForCreators from "../Components/Influencer/Influencer-platform/forCreators";
 
 const InfluencerPage = () => {
   return (
     <>
       <InfluencerHeroSection />
       <Slider />
+      <ForCreators />
     </>
   );
 };
