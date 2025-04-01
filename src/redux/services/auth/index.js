df63a57ec55584b9b9a554ded61763d83239f202
Marcos8060 <marcosgav80@gmail.com>
2025-04-01 20:10:54 +0300

    fixed error

--- src/redux/services/auth/index.js
+++ src/redux/services/auth/index.js
@@ -9,7 +9,6 @@ export const RegisterBrand = async (brandData) => {
     );
     return response;
   } catch (error) {
-    console.log("AXIOS_ERROR ",error)
     return error.response?.data || error.message;
   }
 };
