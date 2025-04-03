f4a36a9acc8c8244dc48cddf6c1bbe639c193e7e
Marcos Ochieng <marcosochieng@NDK364-MarcosOchieng-mac.local>
2025-04-03 17:39:52 +0300

    mocked influencer dashboard cards

--- src/app/onboarding/brand/dashboard/page.js
+++ src/app/onboarding/brand/dashboard/page.js
@@ -16,17 +16,17 @@ const Dashboard = () => {
           </div>
           <div className="flex gap-4 items-center">
             <p className="font-semibold text-2xl">23</p>
-            <small className="text-secondary font-semibold">All Created Campaigns</small>
+            <small className="text-secondary text-xs font-semibold">All Created Campaigns</small>
           </div>
         </div>
         <div className="bg-white rounded-2xl p-4 text-color shadow-lg text-center space-y-4">
           <div className="flex gap-2 items-center justify-between">
-            <p className="font-light">Live Campaigns</p>
+            <p className="font-light">Pending Applications</p>
             <MdCampaign className="text-xl text-green" />
           </div>
           <div className="flex gap-4 items-center">
             <p className="font-semibold text-2xl">18</p>
-            <small className="text-green font-semibold">Active Campaigns</small>
+            <small className="text-green text-xs font-semibold">Collaboration Requests</small>
           </div>
         </div>
         <div className="bg-white rounded-2xl p-4 text-color shadow-lg text-center space-y-4">
@@ -36,17 +36,17 @@ const Dashboard = () => {
           </div>
           <div className="flex gap-4 items-center">
             <p className="font-semibold text-2xl">7</p>
-            <small className="text-secondary font-semibold">All My Buckets</small>
+            <small className="text-secondary text-xs font-semibold">All My Buckets</small>
           </div>
         </div>
         <div className="bg-white rounded-2xl p-4 text-color shadow-lg text-center space-y-4">
           <div className="flex gap-2 items-center justify-between">
-            <p className="font-light">Influencers</p>
+            <p className="font-light">Engagement Rate</p>
             <FaUsersBetweenLines className="text-xl" />
           </div>
           <div className="flex gap-4 items-center">
-            <p className="font-semibold text-2xl">120</p>
-            <small className="text-primary font-semibold">Influencers In My Repository</small>
+            <p className="font-semibold text-2xl">12%</p>
+            <small className="text-primary text-xs font-semibold">Average Engagement</small>
           </div>
         </div>
       </section>
