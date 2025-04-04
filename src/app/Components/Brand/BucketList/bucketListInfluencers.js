9cef508514e65b00e1eeb4cfa5f22630ec285804
Marcos Ochieng <marcosochieng@NDK364-MarcosOchieng-mac.local>
2025-04-04 14:04:03 +0300

    polished influencer buckets

--- src/app/Components/Brand/BucketList/bucketListInfluencers.js
+++ src/app/Components/Brand/BucketList/bucketListInfluencers.js
@@ -5,26 +5,21 @@ import { FaInstagram } from "react-icons/fa";
 import { IoLogoTiktok } from "react-icons/io5";
 import { FaRegEnvelope } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
-import Footer from "@/app/Components/Footer";
 import { useSelector } from "react-redux";
 import { HiArrowNarrowLeft } from "react-icons/hi";
 import Link from "next/link";
 import "react-loading-skeleton/dist/skeleton.css";
 import Skeleton from "react-loading-skeleton";
+import { FaBoxOpen } from "react-icons/fa";
 
 const BucketListInfluencers = ({ loading }) => {
   const { influencersInBucket } = useSelector((store) => store.bucket);
   return (
     <>
-      <div className="bg-primary px-12 py-8 flex items-center">
-        <h1 className="font-bold text-2xl text-white">
-          Influencers in your Bucket List
-        </h1>
-      </div>
-      <section className="md:px-12 px-4 py-4 bg-background">
+      <section className="py-4 bg-background">
         <Link
           href="/brand/influencer-discovery"
-          className="text-color flex items-center gap-2 my-6"
+          className="text-color flex items-center gap-2"
         >
           <HiArrowNarrowLeft />
           <p className="font-semibold">Back to Buckets</p>
@@ -33,69 +28,86 @@ const BucketListInfluencers = ({ loading }) => {
           <Skeleton
             baseColor="#E6E7EB"
             highlightColor="#f0f0f0"
-            count={4}
+            count={5}
             height={100}
           />
         ) : (
-          <section className="grid md:grid-cols-5 grid-cols-1 gap-4 mb-2">
-            {influencersInBucket.map((data, index) => (
-              <div key={index} className="bg-white shadow-xl rounded-lg p-4">
-                <img
-                  className="h-[180px] w-full object-cover rounded-md mb-1"
-                  src={data.profilePicture}
-                  alt=""
-                />
-                <section className="border-b border-input flex items-center justify-between mb-2">
-                  <div>
-                    <p className="text-sm font-bold">{data.fullName}</p>
-                    <small className="text-color text-xs">
-                      {data.city}, {data.country}
-                    </small>
+          <>
+            {influencersInBucket.length > 0 ? (
+              <section className="grid md:grid-cols-5 grid-cols-1 gap-4 mb-2">
+                {influencersInBucket.map((data, index) => (
+                  <div
+                    key={index}
+                    className="bg-white shadow-xl rounded-lg p-4"
+                  >
+                    <img
+                      className="h-[180px] w-full object-cover rounded-md mb-1"
+                      src={data.profilePicture}
+                      alt=""
+                    />
+                    <section className="border-b border-input flex items-center justify-between mb-2">
+                      <div>
+                        <p className="text-sm font-bold">{data.fullName}</p>
+                        <small className="text-color text-xs">
+                          {data.city}, {data.country}
+                        </small>
+                      </div>
+                      <div>
+                        <p className="text-sm font-bold">10M</p>
+                        <small className="text-color text-xs">Followers</small>
+                      </div>
+                    </section>
+                    <div className="space-y-2">
+                      <section className="flex items-center justify-between">
+                        <div className="flex items-center justify-between gap-4 text-xs">
+                          <FaYoutube className="text-red" />
+                          <FaInstagram />
+                          <IoLogoTiktok />
+                        </div>
+                        <div>
+                          <small className="text-[10px] bg-background rounded-3xl py-1 px-2">
+                            Entertainment
+                          </small>
+                        </div>
+                      </section>
+                      <section className="flex items-center justify-between">
+                        <div className="flex items-center justify-between gap-4 text-xs">
+                          <small className="text-[11px]">
+                            Advertising Price
+                          </small>
+                        </div>
+                        <div>
+                          <small className="text-xs font-bold">$5000</small>
+                        </div>
+                      </section>
+                      <section className="flex items-center justify-between">
+                        <div className="flex items-center justify-between gap-4 text-xs">
+                          <FaRegEnvelope />
+                          <FaHeart />
+                        </div>
+                        <div>
+                          <small className="text-[10px] bg-background rounded py-2 px-2">
+                            Send Message
+                          </small>
+                        </div>
+                      </section>
+                    </div>
                   </div>
-                  <div>
-                    <p className="text-sm font-bold">10M</p>
-                    <small className="text-color text-xs">Followers</small>
-                  </div>
-                </section>
-                <div className="space-y-2">
-                  <section className="flex items-center justify-between">
-                    <div className="flex items-center justify-between gap-4 text-xs">
-                      <FaYoutube className="text-red" />
-                      <FaInstagram />
-                      <IoLogoTiktok />
-                    </div>
-                    <div>
-                      <small className="text-[10px] bg-background rounded-3xl py-1 px-2">
-                        Entertainment
-                      </small>
-                    </div>
-                  </section>
-                  <section className="flex items-center justify-between">
-                    <div className="flex items-center justify-between gap-4 text-xs">
-                      <small className="text-[11px]">Advertising Price</small>
-                    </div>
-                    <div>
-                      <small className="text-xs font-bold">$5000</small>
-                    </div>
-                  </section>
-                  <section className="flex items-center justify-between">
-                    <div className="flex items-center justify-between gap-4 text-xs">
-                      <FaRegEnvelope />
-                      <FaHeart />
-                    </div>
-                    <div>
-                      <small className="text-[10px] bg-background rounded py-2 px-2">
-                        Send Message
-                      </small>
-                    </div>
-                  </section>
+                ))}
+              </section>
+            ) : (
+              <section className="h-[60vh] flex items-center justify-center">
+                <div className="flex flex-col items-center justify-center">
+                  <FaBoxOpen className="text-9xl text-gray" />
+                  <p className="mr-4 text-sm font-light">
+                    No Influencers available in this Bucket
+                  </p>
                 </div>
-              </div>
-            ))}
-          </section>
+              </section>
+            )}
+          </>
         )}
       </section>
-      <Footer />
     </>
   );
 };
