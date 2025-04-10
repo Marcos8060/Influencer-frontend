e156e9dab5ccf4a479c7642c48c6806f9459063b
Marcos Ochieng <marcosochieng@NDK364-MarcosOchieng-mac.local>
2025-04-10 11:16:12 +0300

    adjusted the url

--- src/app/Components/Influencer/profile/instagram-profile.js
+++ src/app/Components/Influencer/profile/instagram-profile.js
@@ -21,7 +21,7 @@ const InstagramProfile = () => {
       const authUrl = response.message;
       // Redirect user to TikTok's authorization page
       const url = 'https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=1630927701133160&redirect_uri=https://influencer-frontend-nu.vercel.app/auth/instagram-callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights';
-      window.location.href =  authUrl
+      window.location.href =  url
     } catch (error) {
       console.error("Instagram auth failed:", error);
     }
