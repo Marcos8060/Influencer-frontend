4bca3b73f6d6a9e5e00c55a7d41d8669d675a4a2
Marcos Ochieng <marcosochieng@NDK364-MarcosOchieng-mac.local>
2025-04-10 10:40:20 +0300

    made instagram url dynamic

--- src/app/Components/Influencer/profile/instagram-profile.js
+++ src/app/Components/Influencer/profile/instagram-profile.js
@@ -21,7 +21,8 @@ const InstagramProfile = () => {
       console.log("INSTAGRAM_RES ",response)
       const authUrl = response.message;
       // Redirect user to TikTok's authorization page
-      window.location.href =  'https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=1630927701133160&redirect_uri=https://influencer-frontend-nu.vercel.app/auth/instagram-callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights';
+      const url = 'https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=1630927701133160&redirect_uri=https://influencer-frontend-nu.vercel.app/auth/instagram-callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights';
+      window.location.href =  authUrl
     } catch (error) {
       console.error("Instagram auth failed:", error);
     }
